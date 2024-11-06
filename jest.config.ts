61c1ced5b1dee591f36972ae6de2ebf26c781bc3
정규재 <wjdrbwo1206@naver.com>
2024-11-06 13:42:53 +0900

    chore: jest config

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable import/no-default-export */
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
+
+export default config;
---
+++ jest.setup.ts
@@ -0,0 +1,3 @@
+/**
+ * This file is automatically run before each test file.
+ */
